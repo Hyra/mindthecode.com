df4121aa12999ae63ba1b6b17bfd70ef698b011e
Stef van den Ham <stef@mindthecode.com>
2015-08-02 14:00:10 +0200

    non-www force

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.use(compression());
 
 app.use(express.static(__dirname + '/build/', { maxAge: oneDay }));
 
+if(process.env.NODE_ENV === 'production') {
+  app.get('/*', function(req, res, next) {
+    if (req.headers.host.match(/^www/) !== null ) {
+      res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+    } else {
+      next();     
+    }
+  });
+}
+
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
     console.log('listening on', port);

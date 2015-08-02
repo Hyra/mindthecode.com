45e6402b75d2a318f6db333d2e8d0262ea9b641b
Stef van den Ham <stef@mindthecode.com>
2015-08-02 14:27:09 +0200

    non www

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var compression = require('compression');
 var app = express();
 var oneDay = 86400000;
 
-app.use(compression());
-
-app.use(express.static(__dirname + '/build/', { maxAge: oneDay }));
-
 app.all(/.*/, function(req, res, next) {
   var host = req.header("host");
   if (host.match(/^www\..*/i)) {
@@ -15,6 +11,10 @@ app.all(/.*/, function(req, res, next) {
     res.redirect(301, "http://www." + host);
   }
 });
+
+app.use(compression());
+
+app.use(express.static(__dirname + '/build/', { maxAge: oneDay }));
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() {

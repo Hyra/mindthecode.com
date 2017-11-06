8cc6a61609d7f11a6df45303ff67cc76ee0e4e4c
Hyra <stef@mindthecode.com>
2017-11-06 09:42:14 +0100

    Layout tweaks

---
+++ source/_drafts/taking-screenshots-of-a-website-with-nodejs.md
@@ -0,0 +1,7 @@
+---
+title: Taking screenshots of a website with NodeJS
+date: 2017-04-23 14:50:52
+tags: nodejs
+---
+
+In fd
--- themes/mtc/source/js/main.js
+++ themes/mtc/source/js/main.js
@@ -1,42 +1,62 @@
 window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
+  var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
 
-    t._e = [];
-    t.ready = function(f) {
-        t._e.push(f);
-    };
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
 
-    return t;
-}(document, "script", "twitter-wjs"));
+  return t;
+})(document, "script", "twitter-wjs");
 
-if(navigator.userAgent.indexOf("Speed Insights") == -1) {
-    (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+if (navigator.userAgent.indexOf("Speed Insights") == -1) {
+  (function(i, s, o, g, r, a, m) {
+    i["GoogleAnalyticsObject"] = r;
+    (i[r] =
+      i[r] ||
+      function() {
+        (i[r].q = i[r].q || []).push(arguments);
+      }), (i[r].l = 1 * new Date());
+    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(
+    window,
+    document,
+    "script",
+    "https://www.google-analytics.com/analytics.js",
+    "ga"
+  );
 
-    - ga("create", "UA-40203772-4", "auto");
-    - ga("send", "pageview");
+  -ga("create", "UA-40203772-4", "auto");
+  -ga("send", "pageview");
 }
 
 if (typeof console === "undefined" || typeof console.log === "undefined") {
-    console = {};
-    console.log = function () { };
+  console = {};
+  console.log = function() {};
 }
 
-console.log("%cMade with ❤ and %c🍺", "color: #333; font-size: 12px; font-weight: bold;", "font-weight: normal");
-console.log("%cYou can follow me on https://twitter.com/stefvdham", "color: #333; font-size: 11px;");
+console.log(
+  "%cMade with ❤ and %c🍺",
+  "color: #333; font-size: 12px; font-weight: bold;",
+  "font-weight: normal"
+);
+console.log(
+  "%cYou can follow me on https://twitter.com/hyra",
+  "color: #333; font-size: 11px;"
+);
 
 var links = document.links;
 
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-   if (links[i].hostname != window.location.hostname) {
-       links[i].target = "_blank";
-   }
+  if (links[i].hostname != window.location.hostname) {
+    links[i].target = "_blank";
+  }
 }

5258f668b6b6d744f4b823cecace48cf4b99fb66
Stef van den Ham <stef@mindthecode.com>
2021-05-10 11:11:10 +0200

    Pagespeed improvements

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="preconnect" href="https://srv.carbonads.net" crossorigin>
-    <title>Mindthecode{{ isset($article) ? ' | ' . $article->title : ''}}</title>
     <link rel="preconnect" href="//www.gstatic.com" crossorigin>
-    <link href="/favicon.png" rel="shortcut icon" type="image/x-icon" />
+    <link rel="preconnect" href="https://srv.carbonads.net" crossorigin>
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://cdn.jsdelivr.net">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <style>
+        <?php include(public_path().'/css/app.css'); ?>
+    </style>
+    <title>Mindthecode{{ isset($article) ? ' | ' . $article->title : ''}}</title>
+    <link href="/favicon.png" rel="shortcut icon" type="image/x-icon" />
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
     @include('feed::links')
     @yield('og')
